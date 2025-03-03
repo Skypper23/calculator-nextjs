b7ec8fb7b96a402820d6504676674f1cceb0d8c3
Skypper23 <jonyig29@gmail.com>
2025-03-03 21:45:36 +0000

    return

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,15 @@
 import Calculator from "./calculator";
-import Document, { Html, Head } from "next/document";
 
-class myDocument extends Document{
-  return(){
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
     <html lang="pt-pt">
       <body>
         <Calculator/>
       </body>
     </html>
-  };
+  );
 }
-
-export default myDocument;
